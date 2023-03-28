a5657ec405727a12e2d89caa9c8a8e7a31bcc9df
Aleksandr <alexlev83@list.ru>
2023-03-28 12:07:33 +0300

    Задача 10, 13, 15

--- Program.cs
+++ Program.cs
@@ -176,15 +176,73 @@
 
 // Task 2
 
-Console.Write("Введите число N: ");
-int числоN = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите число N: ");
+//int числоN = Convert.ToInt32(Console.ReadLine());
 
-int N = 8;
+//int N = 8;
 
-if(N % 2 == 0) N = 2;
-if(N % 2 == 0) N = 4;
-if(N % 2 == 0) N = 6;
-if(N % 2 == 0) N = 8;
-{
-Console.WriteLine("четные числа " +2+ "," +4+ "," +6+ "," +8);
-}
+//if(N % 2 == 0) N = 2;
+//if(N % 2 == 0) N = 4;
+//if(N % 2 == 0) N = 6;
+//if(N % 2 == 0) N = 8;
+//{
+//Console.WriteLine("четные числа " +2+ "," +4+ "," +6+ "," +8);
+//}
+
+
+//Задача 10 
+
+//Console.WriteLine("Введите число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+    //if(num >= 100)
+    //if(num < 1000)
+    //{
+       // int ten = (num % 100 - num % 10) /10;
+      //Console.Write($"you number: " + ten);
+    //}
+   // else 
+    //{
+     //  Console.Write($"you number not a three-digit number ");
+     // }
+
+
+   // Задача 13 Напишите программу, которая выводит третью цифру 
+   //           заданного числа или сообщает, что третьей цифры нет.
+
+   //Console.WriteLine("Введите число: ");
+   //int num = Convert.ToInt32(Console.ReadLine());
+     
+   //  if(num > 100)
+   //  {
+   //  int units = (num % 10);
+  //   Console.Write($"Ваше число "  + units);
+  //   }
+  //   else
+   //  {
+  //        Console.Write($"третьей цифры нет ");
+   //      }
+
+// Задача 15 Напишите программу, которая принимает на вход цифру, обозначающую 
+//           день недели, и проверяет, является ли этот день выходным.
+
+//Console.WriteLine("Введите цифру дня недели: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+   //if(num >= 6) 
+   //if(num <= 7)
+   //{
+    //Console.WriteLine("Выходной день");
+   //}
+   // if(num <= 5) 
+    //if(num >= 1)
+    //{
+    //Console.WriteLine("Будний день");
+    //}
+ 
+
+
+
+
+
+
