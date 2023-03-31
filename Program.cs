e3ad03ba0da01751b33ba0b0f936794f64b07ced
Aleksandr <alexlev83@list.ru>
2023-03-31 09:40:57 +0300

    Задача 19, 21, 23

--- Program.cs
+++ Program.cs
@@ -1,81 +1,4 @@
-﻿
-//Задача 10 
-
-//Console.WriteLine("Введите число: ");
-//int num = Convert.ToInt32(Console.ReadLine());
-
-    //if(num >= 100)
-    //if(num < 1000)
-    //{
-       // int ten = (num % 100 - num % 10) /10;
-      //Console.Write($"you number: " + ten);
-    //}
-   // else 
-    //{
-     //  Console.Write($"you number not a three-digit number ");
-     // }
-
-
-   // Задача 13 Напишите программу, которая выводит третью цифру 
-   //           заданного числа или сообщает, что третьей цифры нет.
-
-   //Console.WriteLine("Введите число: ");
-   //int num = Convert.ToInt32(Console.ReadLine());
-     
-   //  if(num > 100)
-   //  {
-   //  int units = (num % 10);
-  //   Console.Write($"Ваше число "  + units);
-  //   }
-  //   else
-   //  {
-  //        Console.Write($"третьей цифры нет ");
-   //      }
-
-// Задача 15 Напишите программу, которая принимает на вход цифру, обозначающую 
-//           день недели, и проверяет, является ли этот день выходным.
-
-//Console.WriteLine("Введите цифру дня недели: ");
-//int num = Convert.ToInt32(Console.ReadLine());
-
-   //if(num >= 6) 
-  // if(num <= 7)
-  //{
-  //  Console.WriteLine("Выходной день");
-  // }
-   // if(num <= 5) 
-   // if(num >= 1)
-   // {
-  //  Console.WriteLine("Будний день");
-  //  }
-
-  // 30.03.23 Семинар 3
-  // Задача 1  Напишите программу, которая по заданному номеру четверти, 
-  //  показывает диапазон возможных координат точек в этой четверти (x и y).
-
-
-//void ShowRange(int guadrant)
-//{
-  // if (guadrant == 1) Console.WriteLine("x > 0 and y > 0");
-  // else  if (guadrant == 2) Console.WriteLine("x < 0 and y > 0");
-  // else  if (guadrant == 3) Console.WriteLine("x < 0 and y < 0");
-  // else  if (guadrant == 4) Console.WriteLine("x > 0 and y < 0");
-  // else Console.WriteLine ("guadrant doesnt exist");
-//}
-//Console.Write("input a number of guadrant: ");
-//int quandNum = Convert.ToInt32(Console.ReadLine());
-//ShowRange(quandNum);
-
-//  Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 
-//и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
-
-//Напишите программу, которая принимает на вход координаты 
-//двух точек и находит расстояние между ними в 2D пространстве.
-
-//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу
- //квадратов чисел от 1 до N.
-
-// Задача 19
+﻿// Задача 19
 
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, 
 // является ли оно палиндромом.
@@ -90,10 +13,31 @@
 
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
-void ShowDistance(double x, double y, double)
+double ShowDistance (double x1, double y1, double z1,double x2, double y2, double z2)
 {
+double disx = x2 - x1;
+double disy = y2 - y1;
+double disz = z2 - z1;
+
+double disxyz = Math.Sqrt(disx * disx + disy * disy + disz * disz);
+return disxyz;
 
 }
+Console.Write("Ведите координату x1: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите координату y2: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите координату z3: ");
+double y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите координату x2: ");
+double y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите координату y2: ");
+double z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите координату z2: ");
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ShowDistance(x1, x2, y1, y2, z1, z2));
+
 
 
 
@@ -104,9 +48,6 @@ void ShowDistance(double x, double y, double)
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
-
-
-
  
 
 
